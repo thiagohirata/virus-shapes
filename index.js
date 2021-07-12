119ae875e12ceea0378b02c424fc8d56159811ad
Thiago Hirata <thiago.hirata@gmail.com>
2021-07-12 00:41:20 -0300

    ajustes

--- index.js
+++ index.js
@@ -40,31 +40,33 @@ if (svg.g[0].g) {
     shapes.push(...svg.g[0].g.map(group => ({ name: group['@_id'], path_strs: group.path.map(shape => shape['@_d']) })))
 }
 // const shapes = [
-//     { name: 'LetterD', d: "m -2.6458333,-13.229167 c 7.9375,0 14.5520833,5.29167 14.5520833,13.2291700333333 C 11.90625,9.2604133 6.6145837,14.552083 -2.6458333,14.552083 h -7.9374997 v -27.78125 z" },
-//     { name: 'LetterC', d: "m 10.583333,-10.4775 c 0,0 -5.2916651,-2.487082 -9.2604151,-2.487083 -3.96875,-1e-6 -13.2291679,1.243542 -13.2291679,13.67895758 0,12.43541842 9.2604179,13.67895842 13.2291679,13.67895842 3.96875,0 9.2604151,-2.487083 9.2604151,-2.487083" },
+//     { name: 'Debug', path_strs: ["M -10,-12 V 12"] },
 // ]
 
 const distanceOf = function (a, b) {
     return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
 }
 
-const BASE_RATE = 8
+const BASE_RATE = 6
 const SCALE = .3
+const MIN_DIST = 4
+const ADJUST = .1
+
 for (let i = 0; i < shapes.length; i++) {
     const { name, path_strs } = shapes[i]
     const positions = []
     for (const path_str of path_strs) {
         const totalLength = raphael.getTotalLength(path_str);
-        const pointCount = Math.ceil(totalLength / BASE_RATE)
+        const pointCount = Math.floor((totalLength + ADJUST) / BASE_RATE);
         const rate = totalLength / pointCount
-        for (var c = 0; c <= totalLength; c += rate) {
+        for (var c = 0; c <= totalLength + ADJUST; c += rate) {
             var point = raphael.getPointAtLength(path_str, c);
             const position = {
                 x: point.x,
                 y: point.y * -1,
                 z: 0
             }
-            if (positions.find(p => distanceOf(position, p) < .5 *BASE_RATE) == null) {
+            if (positions.find(p => distanceOf(position, p) < MIN_DIST) == null) {
                 positions.push(position)
             }
 
